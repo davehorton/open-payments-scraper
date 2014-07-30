580a71a0a4e725a4b14fad36abba17bcd51d599a
Dave Horton <daveh@beachdognet.com>
2014-07-30 11:59:58 -0400

    initial working version

--- app.js
+++ app.js
@@ -1,17 +1,17 @@
-'use strict';
-
-var scraper = require('./lib/open-payments-scraper')
-,debug = require('debug')('open-payments')
-,request = require('request')
-,cheerio = require('cheerio') ;
-
-var argv = require('minimist')(process.argv.slice(2));
+var request = require('request').defaults({jar: true, strictSSL: false})  
+var cheerio = require('cheerio') 
+var async = require('async')
+var co = require('co')
+var thunkify = require('thunkify')
+var debug = require('debug')('open-payments')
+var merge = require('merge')
+var argv = require('minimist')(process.argv.slice(2))
+var fs = require('fs')
 
 if( argv.length < 2 || !argv.user || !argv.password ) {
-	return usage() ;
+	console.error('usage: node app --user <username> --password <password> --outdir <folder>') ;
+	return ;
 }
-
-var request = request.defaults({jar: true}) ;
 
 debug('connecting to cms portal...')
 request('https://portal.cms.gov/wps/myportal', function(error, response, body){
@@ -20,7 +20,7 @@ request('https://portal.cms.gov/wps/myportal', function(error, response, body){
 	
 	debug('Accepting terms..') ;
 
-	request({
+	r({
 		uri: 'https://eidm.cms.gov/EIDMLoginApp/userlogin.jsp'
 		,method: 'POST'
 		,form: {
@@ -33,7 +33,7 @@ request('https://portal.cms.gov/wps/myportal', function(error, response, body){
 
 		debug('Logging in...') ;
 
-		request({
+		r({
 			uri: 'https://eidm.cms.gov/oam/server/auth_cred_submit'
 			,method: 'POST'
 			,form: {
@@ -55,7 +55,7 @@ request('https://portal.cms.gov/wps/myportal', function(error, response, body){
 					var href = $('a.navflySub2[href*="/wps/myportal/cmsportal/op/op_reg"]').attr('href') ;
 					if( !href ) throw new Error('did not get expected welcome page') ;
 
-					request('https://portal.cms.gov' + href, function(err, response, body){
+					r('https://portal.cms.gov' + href, function(err, response, body){
 						if( err ) throw err ;
 						if( response.statusCode !== 200 ) throw new Error('response navigating to open payments page %d', response.statusCode);
 
@@ -74,11 +74,11 @@ request('https://portal.cms.gov/wps/myportal', function(error, response, body){
 						formData[form.attr('id')] = form.attr('id') ;
 						formData['javax.faces.ViewState'] = 'j_id1:j_id2' ;
 
-						request({
+						r({
 							uri: 'https://portal.cms.gov' + form.attr('action')
 							,method: 'POST'
 							,form: formData
-						}, function(err, response, body){
+						}, function(err, response, body) {
 							if( err ) throw err ;
 							//debug('response to selecting review and dispute was %d', response.statusCode) ;
 							//debug('response html: %s', body) ;
@@ -87,82 +87,261 @@ request('https://portal.cms.gov/wps/myportal', function(error, response, body){
 							var options = $('.ProfileResults .FormRow.grid_400').eq(0).find('select option') ;
 							debug('Found %d physicians', options.length -1 ) ;
 
-
-							//find form level data we'll need
-							var button = $('input[value="Show Records"]') ;
-							var buttonName = button.attr('name') ;
-							var form = button.closest('form') ;
-							var hidden = form.find('input[name="javax.faces.ViewState"]') ;
-
-							var formData = {} ;
-							var formId = form.attr('id')
-							formData[formId] = formId ;
-							formData[buttonName] = 'Show Records' ;
-							formData['javax.faces.ViewState'] = hidden.attr('value') ;
-
+							var hcps = [] ;
 							options.each( function(idx, el){
 								if( 0 == idx ) return ;
-
-								var name = $(this).html() ;
-								var value = $(this).attr('value') ;
-								debug('Getting data for %s...', name) ;
-
-								formData[formId+':orgSelected'] = value ;
-								formData[formId+':PaymentYear'] = '2013' ;
-
-								//debug('sending POST with form data: ', formData) ;
-								//debug('sending POST with uri: %s', 'https://portal.cms.gov' + form.attr('action') )
-
-								request({
-									uri: 'https://portal.cms.gov' + form.attr('action')
-									,method: 'POST'
-									,form: formData
-									//,headers: {
-									//	'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.153 Safari/537.36'
-									//}
-								}, function(err, response, body){
-									if( err ) throw error ;
-
-									var $ = cheerio.load(body) ;
-									var page = 1 ;
-									var pages = $('.CallOut.fullCallOut table.SearchDataTable tfoot table table tbody tr td')
-									.eq(2)
-									.find('span').html() ;
-									var pos = pages.indexOf(' of ') ;
-									pages = parseInt( pages.slice(pos+4)) ;
-
-									debug('%s has %s of payment records', name, 1 == pages ? '1 page': (pages + ' pages')) ;
-
-									do {
-										var tr = $('.CallOut.fullCallOut table.SearchDataTable > tbody > tr') ;
-										debug('page %d has %d open payment records', page, tr.length)
-
-										tr.each( function(idx) {
-											var company = $(this).find('td').eq(1).find('div') ;
-											var amount = $(this).find('td').eq(8).find('div') ;
-											debug('%s: %s', company.html(), amount.html())
-											//if( 8 == idx ) debug($(this).html())
-										}) ;
-
-										if( page++ < pages ) {
-											//TODO: get next page
-										}
-									} while( page <= pages ) ;
-
+								hcps.push({
+									name: $(this).html() 
+									,org: $(this).attr('value') 
+									,data: []
 								}) ;
 							}) ;
+
+							getAllHcpData( hcps, body, function( err ) {
+								if( err ) throw err ;
+								writeHcpData( hcps ) ;
+							}) ;
 						})
-
-
-					})
+					}) ;
 				}) ;
 			}
-
 		})
 	}) ;
 }) ;
 
+function navigateBack(body, callback) {
+	var $ = cheerio.load(body) ;
+	var form = $('form') ;
+	var button = form.find('input[value="Back"]') ;
+	var hidden = form.find('input[type="hidden"]') ;
+	var action = form.attr('action') ;
 
-function usage() {
-	console.error('usage: node app --user <username> --password <password>') ;
+	//let's get all of the inputs
+	var formData = {} ;
+	var input = form.find('input') ;
+	input.each( function() {
+		var name = $(this).attr('name') ;
+		var value = $(this).attr('value') ;
+		var type = $(this).attr('type') ;
+
+		if( type === 'checkbox' || type === 'image') return ;
+
+		if( type === 'submit' && value !== 'Back') return ;
+
+		formData[name] = value || null;
+	}) ;
+
+	r({
+		uri: 'https://portal.cms.gov' + action
+		,method: 'POST'
+		,form: formData
+	}, function(err, response, body) {
+		if( err ) return callback(err) ;
+		callback(null, body) ;
+	}) ;
+}
+
+function updateFormData( body ) {
+	$ = cheerio.load(body) ;
+	var form = $('form') ;
+	var formId = form.attr('id') ;
+	var button = form.find('input[value="Show Records"]') ;
+	var buttonName = button.attr('name') ;
+	var action = form.attr('action') ;
+
+	var formData = {} ;
+	formData[formId] = formId ;
+	formData[buttonName] = 'Show Records' ;
+	formData['javax.faces.ViewState'] = $('form input[name="javax.faces.ViewState"]').attr('value') ;
+
+	return {
+		form: formData
+		,uri: 'https://portal.cms.gov' + action
+		,formId: formId
+	} ;
+}
+
+function getAllHcpData( hcps, body, cb ) {
+
+	var tasks = [] ;
+	hcps.forEach( function(hcp, idx){
+		if( 0 == idx ) {
+			tasks.push( function(callback) {
+				var obj = updateFormData(body) ;
+				obj.form[obj.formId+':orgSelected'] = hcp.org ;
+				obj.form[obj.formId+':PaymentYear'] = '2013' ;
+				debug('getting data for %s', hcp.name) ;
+				r({
+					uri: obj.uri
+					,method: 'POST'
+					,form: obj.form
+				}, function(err, response, body){
+					if( err ) return callback(err) ;
+					hcp.data = readHcpData( body ) ;
+					if( hcp.data.length ) {
+						debug('hitting back button')
+						navigateBack( body, function(err, body ) {
+							if( err ) return callback(err) ;
+							callback(null, updateFormData(body) ) ;
+						}) ;
+					}
+					else {
+						callback(null, updateFormData(body) ) ;
+					}
+				}) ;
+			}) ;
+		}
+		else {
+			tasks.push( function(obj, callback) {
+				obj.form[obj.formId+':orgSelected'] = hcp.org ;
+				obj.form[obj.formId+':PaymentYear'] = '2013' ;
+
+				//debug('getting data for %s: %s.....%s', hcp.name, loc.slice(0,70) + '...', fd['javax.faces.ViewState']) ;
+				debug('getting data for %s:', hcp.name) ;
+				r({
+					uri: obj.uri
+					,method: 'POST'
+					,form: obj.form
+				}, function(err, response, body){
+					if( err ) return callback(err) ;
+					hcp.data = readHcpData( body ) ;
+					if( hcp.data.length ) {
+						navigateBack( body, function(err, body ) {
+							if( err ) return callback(err) ;
+							callback(null, updateFormData(body) ) ;
+						}) ;
+					}
+					else {
+						callback(null, updateFormData(body) ) ;
+					}
+				}) ;
+			}) ;			
+		}
+	}) ;
+
+	async.waterfall( tasks, function( err, results ){
+		if( err ) return cb(err) ;
+		cb(null) ;
+	})
+}
+function getvs( body ) {
+	var $ = cheerio.load( body ) ;
+	return $('form input[name="javax.faces.ViewState"]').attr('value') ;
+}
+function readHcpData( body ) {
+
+	if( -1 !== body.indexOf('There are no payments or other transfers of value') ) {
+		debug('no payments found') ;
+		return [] ;
+	}
+
+	var $ = cheerio.load(body) ;
+	var page = 1 ;
+	var pages = $('.CallOut.fullCallOut table.SearchDataTable tfoot table table tbody tr td')
+	.eq(2)
+	.find('span').html() ;
+	if( !pages ) {
+		debug('WARNING: unexpected page format; unable to find Page <> of <>') ;
+		return [] ;
+	}
+
+	var pos = pages.indexOf(' of ') ;
+	pages = parseInt( pages.slice(pos+4)) ;
+
+	debug('%s of payment records found', 1 == pages ? '1 page': (pages + ' pages')) ;
+
+	var data = [] ;
+	do {
+		var tr = $('.CallOut.fullCallOut table.SearchDataTable > tbody > tr') ;
+		debug('page %d has %d open payment records', page, tr.length)
+
+		tr.each( function(row) {
+			var payment = [] ;
+			$(this).find('td').each( function(col) {
+				if( 0 == col || 17 == col || 3 == col) return ;
+				payment.push( $(this).find('div').html() ) ;
+			}) ;
+			data.push( payment ) ;
+		}) ;
+
+		if( page++ < pages ) {
+			//TODO: get next page
+		}
+	} while( page <= pages ) ;
+
+	return data ;
+}
+
+function r( opts, callback ) {
+
+	if( typeof opts === 'string') opts = {uri: opts, method: 'GET'} ;
+	
+	request(opts, function( err, response, body ) {
+		if( argv.debug_files ) {
+			logOutput( err, response, body, opts, callback ) ;
+		} else callback( err, response, body ) ;
+	}) ;
+}
+
+var fileNum = 0 ;
+function logOutput( err, response, body, opts, callback ) {
+	fileNum++ ;
+	async.waterfall([
+			function stat( cb ) {
+				fs.stat( argv.debug_files, function(err, stats){
+					cb(null, stats) ;
+				}) ;
+			}
+			,function mkdir(stats, cb) {
+				if( stats && stats.isDirectory() ) return cb(null) ;
+				if( stats ) return cb(argv.debug_files + ' is not a directory') ;
+				fs.mkdir( argv.debug_files, function(err){
+					if( err ) return cb('failure creating debug directory: ' + err) ;
+					cb(null)
+				}) ;
+			}
+			,function writeBody(cb) {
+				var content ;
+				if( !err ) {
+					$ = cheerio.load(body) ;
+					var insert = '<div><p>This page retrieved with ' + opts.method + ' ' + opts.uri + '</p></div>' ;
+					if( opts.form ) {
+						insert += '<div><p>Form data passed: <pre>' + JSON.stringify(opts.form) + '</pre></p></div>' ;
+					}
+					$('body').prepend(insert); 
+					content = $.html() ;
+				}
+				else content = err ;
+				fs.writeFile(argv.debug_files + '/' + fileNum + '.html', content, function(err) {
+					if( err ) return cb(err) ;
+					cb(null);
+				})
+			}
+
+		], function(err){
+			if( err ) return callback( err ) ;
+			callback(err, response, body) ;
+	})
+}
+
+function writeHcpData( hcps, callback ) {
+	hcps.forEach( function(hcp){
+		var filename = argv.outdir + '/' + hcp.name.toLowerCase().replace(/ /g,'_') + '.csv' ;
+		debug('writing data for %s to %s', hcp.name, filename) ;
+		var stream = fs.createWriteStream(filename);
+		stream.once('open', function(fd){
+			stream.write('organization,record id,category,form of payment,nature of payment,date,amount,delay in publication?,' + 
+				'last modified,current standing,review status,date dispute initiated,last modified by,affirmed,history\n') ;
+			hcp.data.forEach( function(payment){
+				payment.forEach( function(val, idx) {
+					if( 0 != idx ) stream.write(',') ;
+					stream.write('"' + val + '"') ;
+				})
+				stream.write('\n') ;
+			}) ;
+			stream.end() ;
+		}) ;
+	}) ;
+	return ;
+
 }
